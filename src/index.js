91cfd1b838389a736a653166e3e8091d3e2874a0
Allaye <kolade199@gmail.com>
2021-05-26 19:27:07 +0000

    make changes to the template engine

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const path = require('path')
 const express = require('express')
 const get_commit_info = require('./commits')
+const fetch = require('node-fetch')
 
 
 const app = express()
 const publicpath = path.join(__dirname, '../public')
 
 app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 app.use(express.static(publicpath))
 app.use(express.urlencoded({ extended: true }))
 
@@ -17,11 +18,11 @@ app.get('/', (req, res)=>{
 })
 
 app.post('/', async (req, res)=>{
-    res.json(get_commit_info(req.body.username, req.body.reponame))
-    let a = await get_commit_info(req.body.username, req.body.reponame)
-    let b = await a 
-    console.log(b)
-
+    if(req.body.username){
+        const promise = await get_commit_info(req.body.username, req.body.reponame)
+        const data = await promise.json()
+        res.render('home', data)
+    }
 })
 
 
