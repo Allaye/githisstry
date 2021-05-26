ea135cc8dc2cb8825297763551c22d16d70ca726
Allaye <kolade199@gmail.com>
2021-05-26 17:51:23 +0000

    make changes to commit.js

--- src/index.js
+++ src/index.js
@@ -9,20 +9,25 @@ const publicpath = path.join(__dirname, '../public')
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'hbs')
 app.use(express.static(publicpath))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res)=>{
     res.render('home', {name:'kola'})
 })
 
-app.post('/data', (req, res)=>{
-    res.json(req)
-    console.log(req)
+app.post('/', async (req, res)=>{
+    if(req.body.username){
+        let data = await get_commit_info(req.body.username, req.body.reponame)
+        data = await data
+        res.json(data)
+        //return res.render('home')
+    }
+    console.log('no data')
 })
 
 
-const port = process.env.PORT || 3400;
+const port = process.env.PORT || 3000;
 
 app.listen(port, ()=>{
     console.log(`server running on port ${port}`)
